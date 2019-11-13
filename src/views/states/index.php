4c240238f7adc4cbde63aa3c49861c0682bb1d27
jeyroik <jeyroik@gmail.com>
2019-11-13 19:23:01 +0300

    upd view

--- src/views/states/index.php
+++ src/views/states/index.php
@@ -2,11 +2,7 @@
     <div class="card-header">Состояния</div>
     <div class="card-body">
         <div class="row">
-            <div class="col-md-6">
-                <ul class="list-group">
-                    @states
-                </ul>
-            </div>
+            @states
         </div>
     </div>
 </div>
--- src/views/states/item.php
+++ src/views/states/item.php
@@ -1,14 +1,16 @@
-<div class="card">
-    <div class="card-header">@state.title</div>
-    <div class="card-body">
-        @state.description
-    </div>
-    <div class="card-footer">
-        <form action="/states/edit/@state.name">
-            <input class="btn btn-primary" type="submit" value="Редактировать">
-        </form>
-        <form action="/states/delete/@state.name">
-            <input class="btn btn-danger" type="submit" value="Удалить">
-        </form>
+<div class="col-md-3">
+    <div class="card">
+        <div class="card-header">@state.title</div>
+        <div class="card-body">
+            @state.description
+        </div>
+        <div class="card-footer">
+            <form action="/states/edit/@state.name">
+                <input class="btn btn-primary" type="submit" value="Редактировать">
+            </form>
+            <form action="/states/delete/@state.name">
+                <input class="btn btn-danger" type="submit" value="Удалить">
+            </form>
+        </div>
     </div>
 </div>
