0a4fe884e6c9b589f02ab8954826bb95e0f451fa
jeyroik <jeyroik@gmail.com>
2019-11-14 18:35:06 +0300

    one more states view fix

--- src/views/states/item.php
+++ src/views/states/item.php
@@ -1,4 +1,9 @@
-<li class="list-group-item">@state.title (@state.description)
-    <a href="/states/edit/@state.name">Редактировать</a> 
-    <a href="/states/delete/@state.name">Удалить</a>
+<li class="list-group-item">
+    <div class="row">
+        <div class="col-md-3">@state.title (@state.description)</div>
+        <div class="col-md-3">
+            <a href="/states/edit/@state.name">Редактировать</a>
+            <a href="/states/delete/@state.name">Удалить</a>
+        </div>
+    </div>
 </li>
