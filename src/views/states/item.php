ca2d30ba471f49547b87d9c12b4885d5acd7aceb
jeyroik <jeyroik@gmail.com>
2019-11-14 18:32:47 +0300

    fix states view

--- src/views/states/item.php
+++ src/views/states/item.php
@@ -1,8 +1,4 @@
 <li class="list-group-item">@state.title (@state.description)
-    <form action="/states/edit/@state.name">
-        <input class="btn btn-primary" type="submit" value="Редактировать">
-    </form>
-    <form action="/states/delete/@state.name">
-        <input class="btn btn-danger" type="submit" value="Удалить">
-    </form>
+    <a href="/states/edit/@state.name">Редактировать</a> 
+    <a href="/states/delete/@state.name">Удалить</a>
 </li>
