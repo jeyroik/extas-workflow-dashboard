433dd28e1d74917b9ffe13e1fdc394afadc0749f
jeyroik <jeyroik@gmail.com>
2019-11-18 17:40:03 +0300

    upd view

--- src/views/transitions/item.php
+++ src/views/transitions/item.php
@@ -1,6 +1,6 @@
 <li class="list-group-item">
     <div class="row">
-        <div class="col-md-3">@transition.title (@transition.description)</div>
+        <div class="col-md-3" title="@transition.description">@transition.title</div>
         <div class="col-md-3">
             @transition.state_from &rarr; @transition.state_to
         </div>
