8b577984134dbcdd5c349c4064e44e5dcb875b08
jeyroik <jeyroik@gmail.com>
2019-11-19 12:29:48 +0300

    added edit/delete transitions btns

--- src/views/transitions/item.php
+++ src/views/transitions/item.php
@@ -4,5 +4,9 @@
         <div class="col-md-3">
             @transition.state_from &rarr; @transition.state_to
         </div>
+        <div class="col-md-3">
+            <a href="/transitions/edit/@transition.name">Редактировать</a>
+            <a href="/transitions/delete/@transition.name">Удалить</a>
+        </div>
     </div>
 </li>
