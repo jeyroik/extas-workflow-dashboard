6b80adbbe9ab989f90b09058083eb41c4290cf79
jeyroik <jeyroik@gmail.com>
2019-11-19 12:34:04 +0300

    added tips for edit fields

--- src/views/states/edit.php
+++ src/views/states/edit.php
@@ -2,8 +2,8 @@
     <form action="/states/save/@state.name" method="post">
         <div class="row">
             <div class="col-md-6">
-                <input class="inline-input form-control" name="title" type="text" value="@state.title"/>
-                <input class="inline-input form-control" name="description" type="text" value="@state.description"/>
+                <input title="Название" placeholder="Название" class="inline-input form-control" name="title" type="text" value="@state.title"/>
+                <input title="Описание" placeholder="Описание" class="inline-input form-control" name="description" type="text" value="@state.description"/>
             </div>
             <div class="col-md-3">
                 <input type="submit" value="Сохранить" class="btn btn-primary">
--- src/views/transitions/edit.php
+++ src/views/transitions/edit.php
@@ -1,13 +1,13 @@
 <li class="list-group-item">
     <form action="/transitions/save/@transition.name" method="post">
         <div class="row">
-            <div class="col-md-6" title="@transition.description">
-                <input class="inline-input form-control" name="title" type="text" value="@transition.title"/>
-                <input class="inline-input form-control" name="description" type="text" value="@transition.description"/>
+            <div class="col-md-6">
+                <input title="Название" placeholder="Название" class="inline-input form-control" name="title" type="text" value="@transition.title"/>
+                <input title="Описание" placeholder="Описание" class="inline-input form-control" name="description" type="text" value="@transition.description"/>
             </div>
             <div class="col-md-3">
-                <input class="inline-input form-control" name="title" type="text" value="@transition.state_from"/> &rarr;
-                <input class="inline-input form-control" name="description" type="text" value="@transition.state_to"/>
+                <input title="Из статуса" placeholder="Из статуса" class="inline-input form-control" name="title" type="text" value="@transition.state_from"/> &rarr;
+                <input title="В статус" placeholder="В статус" class="inline-input form-control" name="description" type="text" value="@transition.state_to"/>
             </div>
             <div class="col-md-3">
                 <input type="submit" value="Сохранить" class="btn btn-primary">
