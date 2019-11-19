e8233c3cbc95e37beec0a88f94cf43e03b43b61e
jeyroik <jeyroik@gmail.com>
2019-11-19 14:49:07 +0300

    upd view

--- src/views/transitions/edit.php
+++ src/views/transitions/edit.php
@@ -6,8 +6,8 @@
                 <input title="Описание" placeholder="Описание" class="inline-input form-control" name="description" type="text" value="@transition.description"/>
             </div>
             <div class="col-md-3">
-                <input title="Из статуса" placeholder="Из статуса" class="inline-input form-control" name="state_from" type="text" value="@transition.state_from"/> &rarr;
-                <input title="В статус" placeholder="В статус" class="inline-input form-control" name="state_to" type="text" value="@transition.state_to"/>
+                @transition.state_from
+                @transition.state_to
             </div>
             <div class="col-md-3">
                 <input type="submit" value="Сохранить" class="btn btn-primary">
