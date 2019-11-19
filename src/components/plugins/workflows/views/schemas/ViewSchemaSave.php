9e3d1808b8f8e3e8e8200150fa3c76ce00cf8f50
jeyroik <jeyroik@gmail.com>
2019-11-19 13:27:20 +0300

    copypaste fix

--- src/components/plugins/workflows/views/schemas/ViewSchemaSave.php
+++ src/components/plugins/workflows/views/schemas/ViewSchemaSave.php
@@ -1,5 +1,5 @@
 <?php
-namespace extas\components\plugins\workflows\views;
+namespace extas\components\plugins\workflows\views\schemas;
 
 use extas\components\dashboards\DashboardView;
 use extas\components\plugins\Plugin;
