ce0357a061ec21f7140e49d6c5e49f4c0f02ffaa
jeyroik <jeyroik@gmail.com>
2019-11-19 13:34:33 +0300

    fix view

--- src/views/schemas/edit.php
+++ src/views/schemas/edit.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="col-md-3">
+    <div class="col-md-6 col-md-offset-3">
         <div class="card">
             <form action="/schema/save/@schema.name" method="post">
                 <div class="card-header">
