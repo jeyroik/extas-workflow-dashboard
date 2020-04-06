ee2e73df8aee5ea042f716b7933a09ebc68af10f
jeyroik <jeyroik@gmail.com>
2020-04-06 19:24:47 +0300

    tests

--- tests/dashboards/DashboardListTest.php
+++ tests/dashboards/DashboardListTest.php
@@ -24,18 +24,24 @@ class DashboardListTest extends TestCase
     public function testRender()
     {
         $list = new DashboardList([
+            DashboardList::FIELD__NAME => 'test_list',
             DashboardList::FIELD__TITLE => 'Test title',
             DashboardList::FIELD__SELECTED => 'test',
             DashboardList::FIELD__ITEMS => [
                 $this->buildItem([
                     'name' => 'test',
-                    'title' => 'Test item'
+                    'title' => 'Test item',
+                    'description' => 'Test description'
                 ])
             ]
         ]);
 
         $result = $list->render();
+        $mustBe = '<select name="test_list" title="Test title" class="form-control">
+    <option value="test" title="Test description" selected>Test item</option>
+</select>';
         $this->assertNotEmpty($result);
+        $this->assertEquals($mustBe, $result);
     }
 
     /**
