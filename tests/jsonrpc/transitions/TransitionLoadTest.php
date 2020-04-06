008edf6ab0f8dc6953a2d61216e5e35f60a6ba6a
jeyroik <jeyroik@gmail.com>
2020-04-06 14:06:00 +0300

    added transition load test

--- tests/jsonrpc/transitions/TransitionLoadTest.php
+++ tests/jsonrpc/transitions/TransitionLoadTest.php
@@ -4,25 +4,33 @@ use PHPUnit\Framework\TestCase;
 use extas\interfaces\repositories\IRepository;
 use extas\components\SystemContainer;
 use extas\interfaces\parameters\IParameter;
-use extas\components\workflows\states\WorkflowState;
-use extas\components\jsonrpc\states\StateLoad;
+use extas\components\workflows\transitions\WorkflowTransition;
+use extas\components\jsonrpc\transitions\TransitionLoad;
 use extas\components\servers\requests\ServerRequest;
 use extas\components\servers\responses\ServerResponse;
 use extas\interfaces\jsonrpc\IRequest;
 use extas\interfaces\jsonrpc\IResponse;
 use extas\components\jsonrpc\Request;
 use extas\components\jsonrpc\Response;
+use extas\interfaces\workflows\transitions\IWorkflowTransitionRepository;
+use extas\components\workflows\transitions\WorkflowTransitionRepository;
 use extas\interfaces\workflows\states\IWorkflowStateRepository;
 use extas\components\workflows\states\WorkflowStateRepository;
+use extas\components\workflows\states\WorkflowState;
 use Slim\Http\Response as PsrResponse;
 
 /**
- * Class StateLoadTest
+ * Class TransitionLoadTest
  *
  * @author jeyroik@gmail.com
  */
-class StateLoadTest extends TestCase
+class TransitionLoadTest extends TestCase
 {
+    /**
+     * @var IRepository|null
+     */
+    protected ?IRepository $repo = null;
+
     /**
      * @var IRepository|null
      */
@@ -34,7 +42,13 @@ class StateLoadTest extends TestCase
         $env = \Dotenv\Dotenv::create(getcwd() . '/tests/');
         $env->load();
 
+        $this->repo = new WorkflowTransitionRepository();
         $this->stateRepo = new WorkflowStateRepository();
+
+        SystemContainer::addItem(
+            IWorkflowTransitionRepository::class,
+            WorkflowTransitionRepository::class
+        );
 
         SystemContainer::addItem(
             IWorkflowStateRepository::class,
@@ -44,6 +58,7 @@ class StateLoadTest extends TestCase
 
     public function tearDown(): void
     {
+        $this->repo->delete([WorkflowTransition::FIELD__TITLE => 'test']);
         $this->stateRepo->delete([WorkflowState::FIELD__TITLE => 'test']);
     }
 
@@ -80,22 +95,48 @@ class StateLoadTest extends TestCase
      */
     public function testValid()
     {
-        $operation = new StateLoad();
+        $operation = new TransitionLoad();
         $serverRequest = $this->getServerRequest([
             'data' => [
                 [
-                    WorkflowState::FIELD__NAME => 'test',
-                    WorkflowState::FIELD__TITLE => 'test'
+                    WorkflowTransition::FIELD__NAME => 'test',
+                    WorkflowTransition::FIELD__TITLE => 'test',
+                    WorkflowTransition::FIELD__STATE_FROM => 'from',
+                    WorkflowTransition::FIELD__STATE_FROM => 'to'
                 ],
                 [
-                    WorkflowState::FIELD__NAME => 'test2'
+                    WorkflowTransition::FIELD__NAME => 'already-exists',
+                    WorkflowTransition::FIELD__TITLE => 'test',
+                    WorkflowTransition::FIELD__STATE_FROM => 'from',
+                    WorkflowTransition::FIELD__STATE_FROM => 'to'
+                ],
+                [
+                    WorkflowTransition::FIELD__NAME => 'test2',
+                    WorkflowTransition::FIELD__TITLE => 'test',
+                    WorkflowTransition::FIELD__STATE_FROM => 'unknown-state-from',
+                    WorkflowTransition::FIELD__STATE_FROM => 'to'
+                ],
+                [
+                    WorkflowTransition::FIELD__NAME => 'test2',
+                    WorkflowTransition::FIELD__TITLE => 'test',
+                    WorkflowTransition::FIELD__STATE_FROM => 'from',
+                    WorkflowTransition::FIELD__STATE_FROM => 'unknown-state-to'
                 ]
             ]
         ]);
         $serverResponse = $this->getServerResponse();
 
+        $this->repo->create(new WorkflowTransition([
+            WorkflowTransition::FIELD__NAME => 'already-exists',
+            WorkflowTransition::FIELD__TITLE => 'test'
+        ]));
+
         $this->stateRepo->create(new WorkflowState([
-            WorkflowState::FIELD__NAME => 'test2',
+            WorkflowState::FIELD__NAME => 'from',
+            WorkflowState::FIELD__TITLE => 'test'
+        ]));
+        $this->stateRepo->create(new WorkflowState([
+            WorkflowState::FIELD__NAME => 'to',
             WorkflowState::FIELD__TITLE => 'test'
         ]));
 
@@ -115,7 +156,7 @@ class StateLoadTest extends TestCase
                 IResponse::RESPONSE__VERSION => IResponse::VERSION_CURRENT,
                 IResponse::RESPONSE__RESULT => [
                     'created_count' => 1,
-                    'got_count' => 2
+                    'got_count' => 4
                 ]
             ],
             json_decode($jsonRpcResponse->getPsrResponse()->getBody(), true)
